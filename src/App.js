50d380004de85196bf7565acef6f32362a15a8e0
Ponlaput petlorm <111440048+peechut@users.noreply.github.com>
2023-06-05 22:02:54 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,68 +1,23 @@
-import "./App.css";
-import Header from "./components/Header";
-import AddForm from "./components/AddForm";
-import Item from "./components/Item";
-import { useState,useEffect } from "react";
+import logo from './logo.svg';
+import './App.css';
 
 function App() {
-  const [tasks,setTasks]=useState(JSON.parse(localStorage.getItem("tasks"))||[])
-  const [title,setTitle] = useState("")
-  const [editId,setEditId] = useState(null);
-  const [theme,setTheme] = useState("light");
-  //รูปแบบ 3
-  useEffect(()=>{
-      localStorage.setItem("tasks",JSON.stringify(tasks))
-  },[tasks])
-
-  function deleteTask(id){
-      const result = tasks.filter(item=>item.id !==id)
-      setTasks(result)
-  }
-  function editTask(id){
-      //1
-      setEditId(id)
-      const editTask = tasks.find((item)=>item.id === id)
-      setTitle(editTask.title)
-  }
-  function saveTask(e){
-      e.preventDefault();
-      if(!title){
-          alert("กรุณาป้อนข้อมูลด้วยครับ")
-      }else if(editId){
-        //อัพเดตข้อมูล
-        const updateTask = tasks.map((item)=>{
-          //รายการใดมีรหัสตรงกับรหัสแก้ไข
-            if(item.id === editId){
-                return {...item,title:title}
-            }
-            return item;
-        })
-        setTasks(updateTask)
-        setEditId(null)
-        setTitle("")
-      }else{
-        //เพิ่มรายการใหม่
-        const newTask={
-            id:Math.floor(Math.random()*1000),
-            title:title
-        }
-        setTasks([...tasks,newTask])
-        setTitle("")
-      }
-  }
   return (
-    <div className={"App "+theme}>
-      <Header theme={theme} setTheme={setTheme}/>
-      <div className="container">
-        <AddForm title={title} setTitle={setTitle} saveTask={saveTask} editId={editId}/>
-        <section>
-            {
-              tasks.map((data)=>(
-                <Item key={data.id} data={data} deleteTask={deleteTask} editTask={editTask}/>
-              ))
-            }
-        </section>
-      </div>
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
     </div>
   );
 }
