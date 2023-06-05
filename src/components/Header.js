3f22e854d5b7d165a5c2215ee1c3c132749e64fe
Ponlaput petlorm <111440048+peechut@users.noreply.github.com>
2023-06-05 22:08:42 +0700

    Create Header.js

---
+++ src/components/Header.js
@@ -0,0 +1,13 @@
+export default function Header(){
+    return(
+        <header>
+            <div className="logo">
+                <span>Task Management</span>
+            </div>
+            <div className="theme-container">
+                <span>โหมดกลางคืน</span>
+                <span>สลับ</span>
+            </div>
+        </header>
+    )
+}
